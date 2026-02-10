f73fe2304efb1ee7119adffe3b05528d6cb8afa2
alfredoprp <alfredobarrezu2005@gmail.com>
2026-02-10 02:57:13 -0500

    ya por fin vale

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,9 +1,5 @@
 @props(['value'])
 
-<<<<<<< HEAD
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
-=======
 <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-100']) }}>
->>>>>>> origin/feature/perfil-oscuro-calendario
     {{ $value ?? $slot }}
 </label>
